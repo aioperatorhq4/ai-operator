971af7351bc57efaf6f8523bbfa4a31108a0a6f6
aioperatorhq4 <aioperatorhq@protonmail.com>
2026-06-17 09:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import os
 import feedparser
+from supabase import create_client
+
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 rss_url = "http://feeds.bbci.co.uk/news/rss.xml"
-
 feed = feedparser.parse(rss_url)
 
-print("Latest Articles:")
-
 for article in feed.entries[:5]:
-    print(article.title)
+    data = {
+        "title": article.title,
+        "slug": article.title.lower().replace(" ", "-"),
+        "summary": getattr(article, "summary", ""),
+        "content": getattr(article, "summary", ""),
+        "source": "BBC",
+        "category": "World"
+    }
+
+    supabase.table("articles").insert(data).execute()
+
+print("Articles inserted successfully")
